71ede8ac4de07f26445a42d82b9b19b673937a99
Salman Muin Kayser Chishti <32169182+salmanmkc@users.noreply.github.com>
2019-02-07 22:31:27 +0000

    Add files via upload

---
+++ Hacker Rank/MiniMaxSum/ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr)
+    {
+        long total = 0;
+        for (int x = 0; x < arr.Length; x++)
+        {
+            total += arr[x];
+        }
+        long min = total - arr.Max();
+        long max = total - arr.Min();
+        Console.WriteLine(min + " " + max);
+
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        miniMaxSum(arr);
+    }
+}
